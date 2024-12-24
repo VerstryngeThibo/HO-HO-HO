24faa5ca97b9d4cecacf9f162f851f9a1a8c6961
27 <thibo.verstrynge@ugent.be>
2024-12-24 15:58:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const video2 = document.getElementById("video2");
 
 // Helperfunctie om naar de volgende video te gaan
 function playNextVideo(currentVideo, nextVideo) {
-    currentVideo.classList.add("hidden"); // Verberg huidige video
-    nextVideo.classList.remove("hidden"); // Toon volgende video
-    nextVideo.play(); // Start de volgende video automatisch
+    currentVideo.classList.add("hidden");
+    nextVideo.classList.remove("hidden");
+    if (nextVideo.requestFullscreen) {
+        nextVideo.requestFullscreen();
+    }
+    nextVideo.play();
 }
-
 // Eventlisteners voor het einde van elke video
 video1.addEventListener("ended", () => playNextVideo(video1, bartVideo));
 bartVideo.addEventListener("ended", () => playNextVideo(bartVideo, video2));
